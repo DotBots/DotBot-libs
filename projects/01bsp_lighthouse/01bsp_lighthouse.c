7b09d1067dbfa69d32edf10508a255ef1bb32841
Alexandre Abadie <alexandre.abadie@inria.fr>
2025-11-14 14:38:50 +0100

    projects/01bsp_lighthouse: print base index and counts when data is available

--- projects/01bsp_lighthouse/01bsp_lighthouse.c
+++ projects/01bsp_lighthouse/01bsp_lighthouse.c
@@ -47,5 +47,10 @@ int main(void) {
 
         // the location function has to be running all the time
         db_lh2_process_location(&_lh2);
+        for (uint8_t bs_index = 0; bs_index < LH2_BASESTATION_COUNT; bs_index++) {
+            if (_lh2.data_ready[0][bs_index] == DB_LH2_PROCESSED_DATA_AVAILABLE && _lh2.data_ready[1][bs_index] == DB_LH2_PROCESSED_DATA_AVAILABLE) {
+                printf("BS idx: %d - counts: %u:%u\n", bs_index, _lh2.locations[0][bs_index].lfsr_counts, _lh2.locations[1][bs_index].lfsr_counts);
+            }
+        }
     }
 }
